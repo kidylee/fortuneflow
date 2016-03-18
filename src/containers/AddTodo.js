0d996b6c73bea529562d4068b9ac0a50bada5fd4
kidylee <kidylee@gmail.com>
2016-03-18 16:31:16 -0400

    tried ES2015

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,24 +1,63 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 
-let AddTodo = ({ dispatch }) => {
-  let input
 
-  return (
-    <div>
-      <input ref={node => {
-        input = node
-      }} />
-      <button onClick={() => {
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}>
-        Add Todo
-      </button>
-    </div>
-  )
+
+class AddTodo extends Component {
+
+  constructor(props, context) {
+    super(props, context)
+  }
+
+
+  componentDidMount(){
+    console.log('did componentDidMount')
+    this.input.focus()
+  }
+
+
+
+  render() {
+    let { dispatch, onClick } = this.props
+    return (
+      <div>
+        <input ref={node => {
+          this.input = node
+        }} />
+        <button onClick={() => {
+          // dispatch(addTodo(this.input.value))
+          // this.input.value = ''
+          // this.input.focus()
+          onClick(this.input)
+
+        }}>
+          Add Todo
+        </button>
+      </div>
+    )
+  }
 }
-AddTodo = connect()(AddTodo)
+
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+ function mapDispatchToProps(dispatch) {
+    return {
+      onClick: (el) => {
+        dispatch(addTodo(el.value))
+      }
+    }
+ }
+
+
+
+
+AddTodo = connect(mapStateToProps,mapDispatchToProps)(AddTodo)
 
 export default AddTodo
+
